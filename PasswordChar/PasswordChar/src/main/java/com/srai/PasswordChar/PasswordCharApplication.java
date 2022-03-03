19e7b3ad843b52ecfca1063e5b6254bea00fa103
Stuart Ingram <ingrams2@upmc.com>
2022-03-03 14:54:11 -0500

    Initial baseline

---
+++ PasswordChar/PasswordChar/src/main/java/com/srai/PasswordChar/PasswordCharApplication.java
@@ -0,0 +1,13 @@
+package com.srai.PasswordChar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordCharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordCharApplication.class, args);
+	}
+
+}
